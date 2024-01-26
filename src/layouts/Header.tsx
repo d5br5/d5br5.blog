3337a1659e14686e81ee4d8b5bbf3ce681530657
Doh Kim <ehgud456456@naver.com>
2024-01-26 18:58:02 +0900

    [Feat] header height

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className='flex justify-center'>
+    <header className='flex justify-center h-[60px] items-center'>
       <div className='w-full max-w-[1200px] flex justify-between px-4'>
         <div>
           <Link href='/blog'>blog</Link>
