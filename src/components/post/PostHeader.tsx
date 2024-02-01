61b674bb8e69632f4900397201fd0061bc746f60
Doh Kim <ehgud456456@naver.com>
2024-02-01 18:19:12 +0900

    [Feat] 블로그 글에서 카테고리 메뉴 연결

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { Post } from '@/lib/post';
 import dayjs from 'dayjs';
 import { CalendarDays, Clock3 } from 'lucide-react';
@@ -11,7 +13,9 @@ export const PostHeader = ({ post }: Props) => {
   return (
     <header className='text-center mt-14'>
       <h1 className='mb-5'>{post.title}</h1>
-      <div className='text-base mb-3'>{post.categoryPublicName}</div>
+      <div className='text-base mb-3'>
+        <Link href={`/blog/${post.categoryPath}`}>{post.categoryPublicName}</Link>
+      </div>
       <div className='flex justify-center gap-3 text-sm text-gray-500 dark:text-gray-400'>
         <div className='flex items-center gap-1'>
           <CalendarDays className='w-3.5' />
--- src/layouts/PostListPage.tsx
+++ src/layouts/PostListPage.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import { getCategoryList, getCategoryPublicName, getPostList } from '@/lib/post';
-import { cn } from '@/lib/utils';
 
 interface PostListProps {
   category?: string;
