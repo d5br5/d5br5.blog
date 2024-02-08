2cc6a269494683a198c4e6431ac9343187a77900
Doh Kim <ehgud456456@naver.com>
2024-02-08 11:16:32 +0900

    [Feat] chart hover 게시글 작성

--- src/components/mdx/Image.tsx
+++ src/components/mdx/Image.tsx
@@ -7,9 +7,11 @@ export const Image = ({ src, alt }: ImageProps) => {
   return (
     <>
       <img src={src} alt={alt} className='mx-auto rounded-md mt-8 mb-0' />
-      <span className='w-full block text-center mb-8 mt-2 text-sm text-gray-500 dark:text-gray-400'>
-        {alt}
-      </span>
+      {alt !== '' && (
+        <span className='w-full block text-center mb-8 mt-2 text-sm text-gray-500 dark:text-gray-400'>
+          {alt}
+        </span>
+      )}
     </>
   );
 };
