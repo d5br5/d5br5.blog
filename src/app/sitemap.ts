2889cd02ffedd2809fac0442a223ae5ad444ce4b
d5br5 <ehgud456456@naver.com>
2024-02-10 17:23:36 +0900

    [Feat] robots.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { getSitemapPostList } from '@/lib/post';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const postList = await getSitemapPostList();
-  const baseUrl = 'https://d5br5.dev';
+  const baseUrl = 'https://www.d5br5.dev';
   return [
     {
       url: baseUrl,
--- src/lib/post.ts
+++ src/lib/post.ts
@@ -98,7 +98,7 @@ export const getSortedPostList = async (category?: string) => {
 
 export const getSitemapPostList = async () => {
   const postList = await getPostList();
-  const baseUrl = 'https://d5br5.dev';
+  const baseUrl = 'https://www.d5br5.dev';
   const sitemapPostList = postList.map(({ url }) => ({
     lastModified: new Date(),
     url: `${baseUrl}${url}`,
