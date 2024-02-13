11fb4de696daf251bb2dffa6f401ed4dc6004e11
Doh Kim <ehgud456456@naver.com>
2024-02-13 15:21:07 +0900

    [Feat] responsive toc

--- src/components/post_detail/TableOfContent.tsx
+++ src/components/post_detail/TableOfContent.tsx
@@ -12,16 +12,13 @@ const TableOfContent = ({ post }: Props) => {
   const toc = parseToc(post.content);
   console.log(toc);
   return (
-    <aside className='absolute -top-[100px] left-full h-full not-prose'>
-      <div className='sticky z-10  top-[100px] mt-[100px] ml-[5rem]'>
+    <aside className='absolute -top-[100px] left-full h-full not-prose hidden xl:block '>
+      <div className='sticky z-10  top-[100px] w-[200px] mt-[100px] ml-[5rem]'>
         <div className='px-4 py-2 border-l mb-4'>
           <div className='font-bold mb-1'>on this page</div>
           <ul className='text-xs'>
             {toc.map((item) => (
-              <li
-                key={item.link}
-                className={cn(item.indent === 1 && 'ml-4', 'py-1 whitespace-nowrap')}
-              >
+              <li key={item.link} className={cn(item.indent === 1 && 'ml-4', 'py-1')}>
                 {item.text}
               </li>
             ))}
