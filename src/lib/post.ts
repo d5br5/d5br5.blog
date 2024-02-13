7148c50a40e19fd083d0d72f0af6d76cfd4dbd04
Doh Kim <ehgud456456@naver.com>
2024-02-13 15:27:20 +0900

    [Remove] console.log

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -129,13 +129,11 @@ export const getPostDetail = async (category: string, slug: string) => {
 export const parseToc = (content: string): HeadingItem[] => {
   const regex = /^(##|###) (.*$)/gim;
   const headingList = content.match(regex);
-  console.log(headingList);
   return (
     headingList?.map((heading: string) => ({
       text: heading.replace('##', '').replace('#', ''),
       link: heading
         .replace('# ', '')
-        .replace('#', '')
         .replace('#', '')
         .replace(/[\[\]:!@#$%^&*()+=]/g, '')
         .replace(/ /g, '-')
