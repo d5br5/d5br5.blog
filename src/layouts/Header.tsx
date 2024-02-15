fce2eb7803c83ef6d244168f96310dcb73fdea95
Doh Kim <ehgud456456@naver.com>
2024-02-15 18:44:08 +0900

    [Design] header border bottom

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,7 +4,7 @@ import ThemeSwitch from '@/layouts/theme/Switch';
 
 export const Header = () => {
   return (
-    <nav className='sticky top-0 z-40 flex h-[64px] items-center justify-center pt-1'>
+    <nav className='sticky top-1 z-40 mt-1 flex h-[64px] items-center justify-center border-b bg-background pb-1 shadow-sm'>
       <div className='flex w-full max-w-[1200px] justify-between px-4'>
         <div className='flex items-center'>
           <Link href='/blog'>D5BL5G</Link>
