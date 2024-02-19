290ad823c1086970c99940eb5aeed88eda374f24
Doh Kim <ehgud456456@naver.com>
2024-02-19 15:57:05 +0900

    [Feat] footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,30 @@
+import Link from 'next/link';
+
+import IconGithub from '@/components/icon/Github';
+import IconLinkedin from '@/components/icon/LinkedIn';
+
 export const Footer = () => {
   return (
-    <footer className='flex h-[100px] items-center justify-center'>
-      © 2024 Doh Kim all rights reserved
+    <footer className='flex flex-col items-center justify-center gap-4 pb-16 pt-20 text-center'>
+      <div className='flex justify-center gap-4'>
+        <Link href='https://github.com/d5br5' target='_blank'>
+          <IconGithub
+            className='fill-foreground transition hover:fill-pink-600'
+            height={30}
+            width={30}
+          />
+        </Link>
+        <Link href='https://www.linkedin.com/in/dohkim777' target='_blank'>
+          <IconLinkedin
+            className='fill-foreground transition hover:fill-pink-600'
+            height={30}
+            width={30}
+          />
+        </Link>
+      </div>
+      <div>
+        © 2024. <span className='font-semibold'>Doh Kim</span> all rights reserved.
+      </div>
     </footer>
   );
 };
