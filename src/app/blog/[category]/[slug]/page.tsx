b1526bbf1f48cdbcf0ef3c6520211b9926122a98
Doh Kim <ehgud456456@naver.com>
2024-02-19 12:37:38 +0900

    [Refactor] combine scroll progress bar with header

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { PostBody } from '@/components/post_detail/PostBody';
 import { PostHeader } from '@/components/post_detail/PostHeader';
 import TocSidebar from '@/components/post_detail/TableOfContentSidebar';
 import TocTop from '@/components/post_detail/TableOfContentTop';
-import ScrollProgressBar from '@/layouts/ScrollProgressBar';
 import { getPostDetail, getPostPaths, parsePostAbstract, parseToc } from '@/lib/post';
 
 type Props = {
@@ -25,19 +24,16 @@ const PostDetail = async ({ params: { category, slug } }: Props) => {
   const post = await getPostDetail(category, slug);
   const toc = parseToc(post.content);
   return (
-    <>
-      {/* <ScrollProgressBar /> */}
-      <div className='prose mx-auto w-full max-w-[750px] px-4 dark:prose-invert'>
-        <PostHeader post={post} />
-        <TocTop toc={toc} />
-        <article className='relative'>
-          <TocSidebar toc={toc} />
-          <PostBody post={post} />
-        </article>
-        <hr />
-        <Giscus />
-      </div>
-    </>
+    <div className='prose mx-auto w-full max-w-[750px] px-4 dark:prose-invert'>
+      <PostHeader post={post} />
+      <TocTop toc={toc} />
+      <article className='relative'>
+        <TocSidebar toc={toc} />
+        <PostBody post={post} />
+      </article>
+      <hr />
+      <Giscus />
+    </div>
   );
 };
 
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,19 +2,20 @@
 
 import Link from 'next/link';
 
+import ScrollProgressBar from '@/components/common/ScrollProgressBar';
 import { useSpyElem } from '@/hook/useSpy';
 import ThemeSwitch from '@/layouts/theme/Switch';
 
 export const Header = () => {
-  const { ref, marginTop } = useSpyElem(64);
+  const { ref, marginTop } = useSpyElem(65);
 
   return (
     <nav
       style={{ marginTop }}
       ref={ref}
-      className='fixed z-40 flex h-[64px] w-full items-center justify-center border-b bg-background shadow-sm'
+      className='fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm'
     >
-      <div className='flex w-full max-w-[1200px] justify-between px-4'>
+      <div className='mt-1 flex h-[64px] w-full max-w-[1200px] items-center justify-between px-4'>
         <div className='flex items-center'>
           <Link href='/blog'>D5BL5G</Link>
         </div>
@@ -22,6 +23,7 @@ export const Header = () => {
           <ThemeSwitch />
         </div>
       </div>
+      <ScrollProgressBar />
     </nav>
   );
 };
