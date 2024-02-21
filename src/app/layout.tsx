8402da0cf24bc7c6493239c7359353aaa2c80ec2
Doh Kim <ehgud456456@naver.com>
2024-02-21 10:53:52 +0900

    [Init] add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { Header } from '@/layouts/Header';
 import { Provider as ThemeProvider } from '@/layouts/theme/Provider';
 import { cn } from '@/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -46,6 +47,7 @@ export default function RootLayout({
         </ThemeProvider>
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
