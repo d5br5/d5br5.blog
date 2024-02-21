736fee785158a96a430797ba13e8647b49b0faae
Doh Kim <ehgud456456@naver.com>
2024-02-21 17:07:24 +0900

    [Fix] change blog desc

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 export const baseDomain = 'https://www.d5br5.dev';
 
 export const blogName = "Doh's Tech Blog";
-export const blogDesc = '개발 과정에서 얻은 지식과 경험을 공유합니다.';
+export const blogDesc = '개발 지식과 경험을 공유합니다.';
 
 export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
