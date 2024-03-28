568398ec09e2253aa910e123858a9278599af80b
Doh Kim <ehgud456456@naver.com>
2024-03-28 14:21:16 +0900

    [Feat] article published time

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 export const baseDomain = 'https://www.d5br5.dev';
 
-export const blogName = "Doh's Tech Blog";
+export const blogName = '김도형 개발 블로그';
 export const blogDesc = '개발 지식과 경험을 공유합니다.';
 
 export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`;
