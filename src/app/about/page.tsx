e928abe51b123fef34d86d8e4407f3e955db24a9
Doh Kim <ehgud456456@naver.com>
2024-07-25 11:22:09 +0900

    [Feat] about page 데이터 초안

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Section } from '@/components/ui/section';
 import { RESUME_DATA } from '@/data/resume-data';
-import { GlobeIcon, MailIcon, PhoneIcon } from 'lucide-react';
+import { GlobeIcon, MailIcon } from 'lucide-react';
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
@@ -35,20 +35,6 @@ export default function AboutPage() {
               </a>
             </p>
             <div className='flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden'>
-              {RESUME_DATA.contact.email ? (
-                <Button className='size-8' variant='outline' size='icon' asChild>
-                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                    <MailIcon className='size-4' />
-                  </a>
-                </Button>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <Button className='size-8' variant='outline' size='icon' asChild>
-                  <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                    <PhoneIcon className='size-4' />
-                  </a>
-                </Button>
-              ) : null}
               {RESUME_DATA.contact.social.map((social) => (
                 <Button key={social.name} className='size-8' variant='outline' size='icon' asChild>
                   <a href={social.url}>
@@ -56,16 +42,18 @@ export default function AboutPage() {
                   </a>
                 </Button>
               ))}
+              {RESUME_DATA.contact.email && (
+                <Button className='size-8' variant='outline' size='icon' asChild>
+                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                    <MailIcon className='size-4' />
+                  </a>
+                </Button>
+              )}
             </div>
             <div className='hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex print:text-[12px]'>
               {RESUME_DATA.contact.email ? (
                 <a href={`mailto:${RESUME_DATA.contact.email}`}>
                   <span className='underline'>{RESUME_DATA.contact.email}</span>
-                </a>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className='underline'>{RESUME_DATA.contact.tel}</span>
                 </a>
               ) : null}
             </div>
@@ -78,14 +66,14 @@ export default function AboutPage() {
         </div>
         <Section>
           <h2 className='text-xl font-bold'>About</h2>
-          <p className='text-pretty font-mono text-sm text-muted-foreground print:text-[12px]'>
+          <p className='whitespace-pre-wrap text-pretty font-mono text-sm text-muted-foreground print:text-[12px]'>
             {RESUME_DATA.summary}
           </p>
         </Section>
         <Section>
           <h2 className='text-xl font-bold'>Work Experience</h2>
-          {RESUME_DATA.work.map((work) => {
-            return (
+          <div className='-mx-3 space-y-3'>
+            {RESUME_DATA.work.map((work) => (
               <Card key={work.company}>
                 <CardHeader>
                   <div className='flex items-center justify-between gap-x-2 text-base'>
@@ -93,18 +81,6 @@ export default function AboutPage() {
                       <a className='hover:underline' href={work.link}>
                         {work.company}
                       </a>
-
-                      <span className='inline-flex gap-x-1'>
-                        {work.badges.map((badge) => (
-                          <Badge
-                            variant='secondary'
-                            className='align-middle text-xs print:px-1 print:py-0.5 print:text-[8px] print:leading-tight'
-                            key={badge}
-                          >
-                            {badge}
-                          </Badge>
-                        ))}
-                      </span>
                     </h3>
                     <div className='text-sm tabular-nums text-gray-500'>
                       {work.start} - {work.end ?? 'Present'}
@@ -117,13 +93,13 @@ export default function AboutPage() {
                   {work.description}
                 </CardContent>
               </Card>
-            );
-          })}
+            ))}
+          </div>
         </Section>
         <Section>
           <h2 className='text-xl font-bold'>Education</h2>
-          {RESUME_DATA.education.map((education) => {
-            return (
+          <div className='-mx-3 space-y-3'>
+            {RESUME_DATA.education.map((education) => (
               <Card key={education.school}>
                 <CardHeader>
                   <div className='flex items-center justify-between gap-x-2 text-base'>
@@ -135,36 +111,32 @@ export default function AboutPage() {
                 </CardHeader>
                 <CardContent className='mt-2 print:text-[12px]'>{education.degree}</CardContent>
               </Card>
-            );
-          })}
+            ))}
+          </div>
         </Section>
         <Section>
           <h2 className='text-xl font-bold'>Skills</h2>
           <div className='flex flex-wrap gap-1'>
-            {RESUME_DATA.skills.map((skill) => {
-              return (
-                <Badge className='print:text-[10px]' key={skill}>
-                  {skill}
-                </Badge>
-              );
-            })}
+            {RESUME_DATA.skills.map((skill) => (
+              <Badge className='print:text-[10px]' key={skill}>
+                {skill}
+              </Badge>
+            ))}
           </div>
         </Section>
 
         <Section className='print-force-new-page scroll-mb-16'>
           <h2 className='text-xl font-bold'>Projects</h2>
           <div className='-mx-3 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2'>
-            {RESUME_DATA.projects.map((project) => {
-              return (
-                <ProjectCard
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  tags={project.techStack}
-                  link={'link' in project ? project.link.href : undefined}
-                />
-              );
-            })}
+            {RESUME_DATA.projects.map((project) => (
+              <ProjectCard
+                key={project.title}
+                title={project.title}
+                description={project.description}
+                tags={project.techStack}
+                link={'link' in project ? project.link.href : undefined}
+              />
+            ))}
           </div>
         </Section>
       </section>
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -2,18 +2,17 @@ import IconGithub from '@/components/icon/Github';
 import IconLinkedin from '@/components/icon/LinkedIn';
 
 export const RESUME_DATA = {
-  name: 'Bartosz Jarocki',
-  initials: 'BJ',
-  location: 'Wrocław, Poland, CET',
-  locationLink: 'https://www.google.com/maps/place/Wrocław',
-  about: 'Detail-oriented Full Stack Engineer dedicated to building high-quality products.',
+  name: '김도형, Doh Kim',
+  initials: 'DH',
+  location: 'Seoul, South Korea, KST',
+  locationLink: 'https://www.google.com/maps/place/seoul',
+  about: 'Detail-oriented Front-end Developer',
   summary:
-    'As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.',
-  avatarUrl: 'https://avatars.githubusercontent.com/u/1017620?v=4',
+    '아이디어를 빠르게 서비스하기 위한 가장 강력한 수단은 웹 개발이라고 생각합니다.\n예술 작품을 만들어낸다는 마음가짐으로 디테일에 주의를 기울입니다.\n다양한 플랫폼에서 활용되는 Javascript에 관심을 갖고 열심히 공부하고 있습니다.',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/40906871?v=4',
   personalWebsiteUrl: 'https://jarocki.me',
   contact: {
-    email: 'bartosz.jarocki@hey.com',
-    tel: '+48530213401',
+    email: 'ehgud456456@naver.com',
     social: [
       {
         name: 'GitHub',
@@ -29,72 +28,31 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: 'Wrocław University of Technology',
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: '2007',
-      end: '2010',
+      school: 'Seoul National University',
+      degree: "Bachelor's Degree in Mechanical Engineering and Business Administration",
+      start: '2015',
+      end: '2022',
     },
   ],
   work: [
     {
-      company: 'Film.io',
-      link: 'https://film.io',
-      badges: ['Remote'],
-      title: 'Software Architect',
-      start: '2024',
+      company: '아데나 소프트웨어 Adena Software',
+      link: 'https://www.adenasoft.com/',
+      title: 'Front-end Developer',
+      start: '2023. 03.',
       end: null,
-      description:
-        'Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js',
+      description: '',
     },
     {
-      company: 'Parabol',
-      link: 'https://parabol.co',
-      badges: ['Remote'],
-      title: 'Senior Full Stack Developer',
-      start: '2021',
-      end: '2024',
-      description:
-        'Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL',
-    },
-    {
-      company: 'Clevertech',
-      link: 'https://clevertech.biz',
-      badges: ['Remote'],
-      title: 'Lead Android Developer → Full Stack Developer',
-      start: '2015',
-      end: '2021',
-      description:
-        'Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL',
-    },
-    {
-      company: 'Jojo Mobile',
-      link: 'https://bsgroup.eu/',
-      badges: [],
-      title: 'Android Developer → Lead Android Developer',
-      start: '2012',
-      end: '2015',
-      description:
-        'Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress',
-    },
-    {
-      company: 'Nokia Siemens Networks',
-      link: 'https://www.nokia.com',
-      badges: [],
-      title: 'C/C++ Developer',
-      start: '2010',
-      end: '2012',
-      description: 'Developed and tested software for LTE base stations',
+      company: '티맥스 핀테크 Tmax Fintech',
+      link: 'https://www.tmax.co.kr/tmaxfintech',
+      title: 'Front-end Developer',
+      start: '2022. 02.',
+      end: '2023. 02.',
+      description: '',
     },
   ],
-  skills: [
-    'JavaScript',
-    'TypeScript',
-    'React/Next.js/Remix',
-    'Node.js',
-    'GraphQL',
-    'Relay',
-    'WebRTC',
-  ],
+  skills: ['Javascript', 'Typescript', 'React.js', 'Vue.js', 'Next.js'],
   projects: [
     {
       title: 'Parabol',
@@ -104,107 +62,6 @@ export const RESUME_DATA = {
         label: 'github.com',
         href: 'https://parabol.co/',
       },
-    },
-    {
-      title: 'Evercast',
-      techStack: ['Lead Frontend Developer', 'TypeScript', 'React', 'Node.js', 'GraphQL'],
-      description:
-        'Creative collaboration platform that combines video conferencing and HD media streaming',
-      link: {
-        label: 'evercast.us',
-        href: 'https://www.evercast.us/',
-      },
-    },
-    {
-      title: 'Consultly',
-      techStack: ['Side Project', 'TypeScript', 'Next.js', 'Vite', 'GraphQL', 'WebRTC'],
-      description: 'A platform to build and grow your online business',
-      link: {
-        label: 'consultly.com',
-        href: 'https://consultly.com/',
-      },
-    },
-    {
-      title: 'Monito',
-      techStack: ['Side Project', 'TypeScript', 'Next.js', 'Browser Extension'],
-      description: 'Browser extension that records everything happening in a web application',
-      link: {
-        label: 'monito.dev',
-        href: 'https://monito.dev/',
-      },
-    },
-    {
-      title: 'Jarocki.me',
-      techStack: ['Side Project', 'Next.js', 'MDX'],
-      description: 'Personal website and blog. Built with Next.js and Notion API',
-      link: {
-        label: 'github.com',
-        href: 'https://jarocki.me/',
-      },
-    },
-    {
-      title: 'Minimal',
-      techStack: ['Side Project', 'Next.js', 'Puppeteer'],
-      description: 'Minimalist calendars, habit trackers, and planners generator',
-      link: {
-        label: 'useminimal.com',
-        href: 'https://useminimal.com/',
-      },
-    },
-    {
-      title: 'Barepapers',
-      techStack: ['Side Project', 'Next.js', 'Puppeteer'],
-      description: 'Generates beautiful wallpapers using random shapes and gradients',
-      link: {
-        label: 'barepapers.com',
-        href: 'https://barepapers.com/',
-      },
-    },
-    {
-      title: 'Year progress',
-      techStack: ['Side Project', 'TypeScript', 'Next.js'],
-      description: 'Tracks current year progress and displays a countdown',
-      link: {
-        label: 'getyearprogress.com',
-        href: 'https://getyearprogress.com/',
-      },
-    },
-    {
-      title: 'Mobile Vikings',
-      techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-      description: 'Android application for leading virtual mobile operator in Poland',
-      link: {
-        label: 'mobilevikings.pl',
-        href: 'https://mobilevikings.pl/',
-      },
-    },
-    {
-      title: 'Howdy',
-      techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-      description: 'Howdy is a place for joining communities you care about',
-      link: {
-        label: 'play.google.com',
-        href: 'https://howdy.co/',
-      },
-    },
-    {
-      title: 'Tastycloud',
-      techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-      description: 'Android application for managing and displaying restaurant menus in kiosk mode',
-      link: {
-        label: 'tastycloud.fr',
-        href: 'https://www.tastycloud.fr/',
-      },
-    },
-    {
-      title: 'Ambit',
-      techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-      description: 'Android application that helps with sharing your contact details',
-    },
-    {
-      title: 'Bim',
-      techStack: ['Lead Android Developer', 'Android', 'Kotlin'],
-      description: 'Android application that helps with booking a table in a restaurants',
     },
     {
       title: 'Canal Digital GO',
