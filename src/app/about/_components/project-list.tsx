ef08bdbd237ef49b663438b4b70c64d577264c5c
Doh Kim <ehgud456456@naver.com>
2024-08-01 16:25:45 +0900

    [Feat] modal layout

--- src/app/about/_components/project-list.tsx
+++ src/app/about/_components/project-list.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 import { ProjectCard } from '@/components/common/project-card';
 import { ProjectBody } from '@/components/project-detail/project-body';
 import * as D from '@/components/ui/dialog';
@@ -10,21 +12,25 @@ interface Props {
 export default function ProjectList({ list }: Props) {
   return (
     <div className='grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2'>
-      {list.map((project) => (
-        <D.Dialog key={project.slug}>
-          <D.DialogTrigger>
-            <ProjectCard
-              title={project.title}
-              description={project.desc}
-              // tags={project.techStack}
-              // link={'link' in project ? project.link.href : undefined}
-            />
-          </D.DialogTrigger>
-          <D.DialogContent>
-            <ProjectBody project={project} />
-          </D.DialogContent>
-        </D.Dialog>
-      ))}
+      <D.Dialog>
+        {list.map((project) => (
+          <Fragment key={project.slug}>
+            <D.DialogTrigger>
+              <ProjectCard
+                title={project.title}
+                description={project.desc}
+                // tags={project.techStack}
+                // link={'link' in project ? project.link.href : undefined}
+              />
+            </D.DialogTrigger>
+            <D.DialogContent className='px-0'>
+              <div className='max-h-[80vh] overflow-y-scroll'>
+                <ProjectBody project={project} />
+              </div>
+            </D.DialogContent>
+          </Fragment>
+        ))}
+      </D.Dialog>
     </div>
   );
 }
--- src/components/project-detail/project-body.tsx
+++ src/components/project-detail/project-body.tsx
@@ -17,34 +17,36 @@ interface Props {
 export const ProjectBody = ({ project }: Props) => {
   return (
     <Suspense fallback={<>Loading...</>}>
-      <MDXRemote
-        source={project.content}
-        options={{
-          mdxOptions: {
-            remarkPlugins: [
-              // 깃허브 Flavored 마크다운 지원 추가 (version downgrade)
-              remarkGfm,
-              // 이모티콘 접근성 향상
-              remarkA11yEmoji,
-              // mdx 1줄 개행 지원
-              remarkBreaks,
-            ],
-            rehypePlugins: [
-              // pretty code block
-              [
-                // @ts-ignore
-                rehypePrettyCode,
-                {
-                  theme: { dark: 'github-dark-dimmed', light: 'github-light' },
-                },
+      <div className='prose px-5'>
+        <MDXRemote
+          source={project.content}
+          options={{
+            mdxOptions: {
+              remarkPlugins: [
+                // 깃허브 Flavored 마크다운 지원 추가 (version downgrade)
+                remarkGfm,
+                // 이모티콘 접근성 향상
+                remarkA11yEmoji,
+                // mdx 1줄 개행 지원
+                remarkBreaks,
               ],
-              // toc id를 추가하고 제목을 연결
-              rehypeSlug,
-            ],
-          },
-        }}
-        components={MdxComponents}
-      />
+              rehypePlugins: [
+                // pretty code block
+                [
+                  // @ts-ignore
+                  rehypePrettyCode,
+                  {
+                    theme: { dark: 'github-dark-dimmed', light: 'github-light' },
+                  },
+                ],
+                // toc id를 추가하고 제목을 연결
+                rehypeSlug,
+              ],
+            },
+          }}
+          components={MdxComponents}
+        />
+      </div>
     </Suspense>
   );
 };
