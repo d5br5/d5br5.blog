b0b269344010171118aea7b2040b4fc334ca8564
Doh Kim <ehgud456456@naver.com>
2024-08-01 15:35:21 +0900

    [Feat] project modal

--- src/app/about/_components/project-list.tsx
+++ src/app/about/_components/project-list.tsx
@@ -1,21 +1,29 @@
 import { ProjectCard } from '@/components/common/project-card';
+import { ProjectBody } from '@/components/project-detail/project-body';
+import * as D from '@/components/ui/dialog';
 import { Project } from '@/config/types';
 
 interface Props {
   list: Project[];
 }
 
-export default async function ProjectList({ list }: Props) {
+export default function ProjectList({ list }: Props) {
   return (
     <div className='grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 print:grid-cols-3 print:gap-2'>
       {list.map((project) => (
-        <ProjectCard
-          key={project.slug}
-          title={project.title}
-          description={project.desc}
-          // tags={project.techStack}
-          // link={'link' in project ? project.link.href : undefined}
-        />
+        <D.Dialog key={project.slug}>
+          <D.DialogTrigger>
+            <ProjectCard
+              title={project.title}
+              description={project.desc}
+              // tags={project.techStack}
+              // link={'link' in project ? project.link.href : undefined}
+            />
+          </D.DialogTrigger>
+          <D.DialogContent>
+            <ProjectBody project={project} />
+          </D.DialogContent>
+        </D.Dialog>
       ))}
     </div>
   );
--- src/components/project-detail/project-body.tsx
+++ src/components/project-detail/project-body.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import { MdxComponents } from '../mdx';
 import { Project } from '@/config/types';
 // @ts-expect-error no types
@@ -14,33 +16,35 @@ interface Props {
 
 export const ProjectBody = ({ project }: Props) => {
   return (
-    <MDXRemote
-      source={project.content}
-      options={{
-        mdxOptions: {
-          remarkPlugins: [
-            // 깃허브 Flavored 마크다운 지원 추가 (version downgrade)
-            remarkGfm,
-            // 이모티콘 접근성 향상
-            remarkA11yEmoji,
-            // mdx 1줄 개행 지원
-            remarkBreaks,
-          ],
-          rehypePlugins: [
-            // pretty code block
-            [
-              // @ts-ignore
-              rehypePrettyCode,
-              {
-                theme: { dark: 'github-dark-dimmed', light: 'github-light' },
-              },
+    <Suspense fallback={<>Loading...</>}>
+      <MDXRemote
+        source={project.content}
+        options={{
+          mdxOptions: {
+            remarkPlugins: [
+              // 깃허브 Flavored 마크다운 지원 추가 (version downgrade)
+              remarkGfm,
+              // 이모티콘 접근성 향상
+              remarkA11yEmoji,
+              // mdx 1줄 개행 지원
+              remarkBreaks,
             ],
-            // toc id를 추가하고 제목을 연결
-            rehypeSlug,
-          ],
-        },
-      }}
-      components={MdxComponents}
-    />
+            rehypePlugins: [
+              // pretty code block
+              [
+                // @ts-ignore
+                rehypePrettyCode,
+                {
+                  theme: { dark: 'github-dark-dimmed', light: 'github-light' },
+                },
+              ],
+              // toc id를 추가하고 제목을 연결
+              rehypeSlug,
+            ],
+          },
+        }}
+        components={MdxComponents}
+      />
+    </Suspense>
   );
 };
