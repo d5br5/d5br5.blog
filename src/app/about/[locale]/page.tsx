30dd743becb8b27370c696269e8f9b23bbb33c2e
Doh Kim <ehgud456456@naver.com>
2024-08-01 14:27:13 +0900

    [Refactor] combine locale

--- src/app/about/[locale]/page.tsx
+++ src/app/about/[locale]/page.tsx
@@ -8,73 +8,106 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card';
 import { Section } from '@/components/ui/section';
 import { RESUME_DATA_EN } from '@/data/resume-data-en';
+import { RESUME_DATA_KO } from '@/data/resume-data-ko';
+import { cn } from '@/lib/utils';
 import { GlobeIcon, MailIcon } from 'lucide-react';
 
-export const metadata: Metadata = {
-  title: `${RESUME_DATA_EN.name} | ${RESUME_DATA_EN.about}`,
-  description: RESUME_DATA_EN.summary,
+const DATAS = {
+  en: {
+    data: RESUME_DATA_EN,
+    aboutClassName: '',
+  },
+  ko: {
+    data: RESUME_DATA_KO,
+    aboutClassName: 'sm:whitespace-pre-wrap whitespace-normal',
+  },
 };
 
-export default function AboutPageEnglish() {
+type Locale = keyof typeof DATAS;
+interface Props {
+  params: {
+    locale: Locale;
+  };
+}
+
+export function generateStaticParams() {
+  return Object.keys(DATAS).map((locale) => ({ locale }));
+}
+
+export function generateMetadata({ params: { locale } }: Props): Metadata {
+  const data = DATAS[locale].data;
+  return {
+    title: `${data.name} | ${data.about}`,
+    description: data.summary,
+  };
+}
+
+export default function AboutPageEnglish({ params: { locale } }: Props) {
+  const RESUME_DATA = DATAS[locale].data;
   return (
     <main className='container relative mx-auto scroll-my-12 overflow-auto p-6 pt-12 sm:p-9 md:p-16 print:p-12'>
       <Section className='mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4'>
         <div className='flex flex-col-reverse items-center justify-between gap-4 sm:flex-row'>
           <div className='flex-1 space-y-1.5 text-center sm:text-start'>
-            <h1 className='mb-4 text-3xl font-bold'>{RESUME_DATA_EN.name}</h1>
+            <h1 className='mb-4 text-3xl font-bold'>{RESUME_DATA.name}</h1>
             <p className='max-w-md text-pretty text-muted-foreground print:text-[12px]'>
-              {RESUME_DATA_EN.about}
+              {RESUME_DATA.about}
             </p>
             <p className='max-w-md items-center text-pretty text-sm text-muted-foreground'>
               <a
                 className='inline-flex items-center gap-x-1.5 align-baseline leading-none hover:underline'
-                href={RESUME_DATA_EN.locationLink}
+                href={RESUME_DATA.locationLink}
                 target='_blank'
               >
                 <GlobeIcon className='size-3' />
-                {RESUME_DATA_EN.location}
+                {RESUME_DATA.location}
               </a>
             </p>
             <div className='flex justify-center gap-x-2 pt-1 text-sm text-muted-foreground sm:justify-start print:hidden'>
-              {RESUME_DATA_EN.contact.social.map((social) => (
+              {RESUME_DATA.contact.social.map((social) => (
                 <Button key={social.name} className='size-8' variant='outline' size='icon' asChild>
                   <a href={social.url} target='_blank'>
                     <social.icon className='size-4' />
                   </a>
                 </Button>
               ))}
-              {RESUME_DATA_EN.contact.email && (
+              {RESUME_DATA.contact.email && (
                 <Button className='size-8' variant='outline' size='icon' asChild>
-                  <a href={`mailto:${RESUME_DATA_EN.contact.email}`}>
+                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
                     <MailIcon className='size-4' />
                   </a>
                 </Button>
               )}
             </div>
             <div className='hidden flex-col gap-x-1 text-sm text-muted-foreground print:flex print:text-[12px]'>
-              {RESUME_DATA_EN.contact.email ? (
-                <a href={`mailto:${RESUME_DATA_EN.contact.email}`}>
-                  <span className='underline'>{RESUME_DATA_EN.contact.email}</span>
+              {RESUME_DATA.contact.email ? (
+                <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                  <span className='underline'>{RESUME_DATA.contact.email}</span>
                 </a>
               ) : null}
             </div>
           </div>
 
           <Avatar className='size-28'>
-            <AvatarImage alt={RESUME_DATA_EN.name} src={RESUME_DATA_EN.avatarUrl} />
-            <AvatarFallback>{RESUME_DATA_EN.initials}</AvatarFallback>
+            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
+            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
         </div>
         <Section>
           <h2 className='text-2xl font-bold'>About</h2>
-          <p className='text-pretty leading-8 text-muted-foreground print:text-[12px]'>
-            {RESUME_DATA_EN.summary}
+          <p
+            className={cn(
+              'text-pretty leading-8 text-muted-foreground print:text-[12px]',
+              DATAS[locale].aboutClassName
+            )}
+          >
+            {RESUME_DATA.summary}
           </p>
         </Section>
         <Section>
           <h2 className='text-2xl font-bold'>Work Experience</h2>
           <div className='space-y-4'>
-            {RESUME_DATA_EN.work.map((work) => (
+            {RESUME_DATA.work.map((work) => (
               <Card key={work.company}>
                 <CardHeader className='mb-3'>
                   <div className='flex flex-col items-start justify-between gap-1 gap-x-2 text-base sm:flex-row sm:items-center'>
@@ -109,7 +142,7 @@ export default function AboutPageEnglish() {
         <Section>
           <h2 className='text-2xl font-bold'>Education</h2>
           <div className='space-y-3'>
-            {RESUME_DATA_EN.education.map((education) => (
+            {RESUME_DATA.education.map((education) => (
               <Card key={education.school}>
                 <CardHeader>
                   <div className='flex flex-col items-start justify-between gap-1 gap-x-2 text-base sm:flex-row sm:items-center'>
@@ -129,7 +162,7 @@ export default function AboutPageEnglish() {
         <Section>
           <h2 className='text-2xl font-bold'>Skills</h2>
           <div className='flex flex-wrap gap-1'>
-            {RESUME_DATA_EN.skills.map((skill) => (
+            {RESUME_DATA.skills.map((skill) => (
               <Badge className='print:text-[10px]' key={skill}>
                 {skill}
               </Badge>
@@ -139,7 +172,7 @@ export default function AboutPageEnglish() {
 
         <Section className='print-force-new-page scroll-mb-16'>
           <h2 className='text-2xl font-bold'>Projects</h2>
-          <ProjectList locale='en' />
+          <ProjectList locale={locale} />
         </Section>
       </Section>
       <LanguageSelector />
