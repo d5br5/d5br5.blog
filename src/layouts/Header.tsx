6932c943bcbc1c0b1cdbb8c99a71e1e3351c2077
Doh Kim <ehgud456456@naver.com>
2024-08-05 10:46:45 +0900

    [Feat] email clipboard

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -27,7 +27,7 @@ export const Header = () => {
     <nav
       style={{ marginTop }}
       ref={ref}
-      className='fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm'
+      className='fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm print:hidden'
     >
       <div className='mt-1 flex h-[64px] w-full max-w-[1200px] items-center justify-between px-4'>
         <div className='flex items-center font-medium'>
